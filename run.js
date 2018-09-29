fb6833696f11c4cbcf59cbcbbd37f7bd080d9fdc
Robert Tenney <rob10esf@gmail.com>
2018-09-29 12:08:05 -0400

    Updated run script

--- run.js
+++ run.js
@@ -2,23 +2,23 @@ const spawn = require('cross-spawn');
 
 const path = require('path');
 
-const SEPARATOR = process.platform === 'win32' ? ';' : ':';
-const env = { ...process.env };
+// const SEPARATOR = process.platform === 'win32' ? ';' : ':';
+// const env = { ...process.env };
 
-env.Path = path.resolve('./node_modules/.bin') + SEPARATOR + env.Path;
+// env.PATH = path.resolve('./node_modules/.bin') + SEPARATOR + env.PATH;
 
 function runSpawnSync(cmd, args) {
   const proc = spawn(cmd, args, {
     cwd: process.cwd(),
-    env,
+    // env,
   });
 
   proc.stdout.on('data', (data) => {
-    console.log(data);
+    console.log(data.toString());
   });
 
   proc.stderr.on('data', (data) => {
-    console.error(data);
+    console.error(data.toString());
   });
 
   proc.on('exit', (code, signal) => {
